d1bed5a706fea9241a9eae10714b56ad8854e14e
Roullito <jmoleins@laposte.net>
2025-07-28 11:07:00 +0200

    Add initial implementation of sum_list function with basic annotations and main script for testing

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic annotations - sum_list."""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums a list of floating-point numbers.
+    """
+    return sum(input_list)
