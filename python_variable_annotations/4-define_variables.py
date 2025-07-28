47173d7bd6ccd0b3d8283275d56457d711417f1e
Roullito <jmoleins@laposte.net>
2025-07-28 11:02:45 +0200

    Add initial implementation of variable annotations and main script for testing

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotations - a, pi, i_understand_annotations, school"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
