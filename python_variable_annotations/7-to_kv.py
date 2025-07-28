6a6d66b440a40039f7df5c9d4c8d31c7e88f89af
Roullito <jmoleins@laposte.net>
2025-07-28 11:18:37 +0200

    Add implementation of to_kv function with basic annotations and main script for testing

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic annotations - to_kv"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """Return a tuple with string k and square of v as float"""
+    return (k, v * v)
