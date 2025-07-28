e07df60ee9a7911a59e8d8f54859695d8939b8ba
Roullito <jmoleins@laposte.net>
2025-07-28 11:22:22 +0200

    Refactor docstring in to_kv function for clarity and consistency

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-"""Basic annotations - to_kv"""
-
+"""Basic annotations - to_kv."""
 
 from typing import Union, Tuple
 
-
 def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
-    """Return a tuple with string k and square of v as float"""
-    return (k, v * v)
+    """
+    Returns a tuple containing the string `k` and the square of `v` as a float.
+
+    Args:
+        k (str): The key.
+        v (Union[float, int]): A number to be squared.
+
+    Returns:
+        Tuple[str, float]: A tuple with the key and the squared value as float.
+    """
+    return (k, float(v * v))
