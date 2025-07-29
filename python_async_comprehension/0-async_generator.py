5e78eac64d672ee2bcebf4e0f9d43c5767302796
Roullito <jmoleins@laposte.net>
2025-07-29 13:43:49 +0200

    Add async_generator and main script for yielding random numbers

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic async syntax"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Asynchronous generator that yields 10 random floating-point numbers.
+
+    This coroutine function generates random numbers between 0 and 10,
+    with a 1-second delay between each yield operation.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
