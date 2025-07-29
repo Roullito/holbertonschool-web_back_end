ed6fe27378d90e86bbfd2a55b0f7b6ebdd3f4a9a
Roullito <jmoleins@laposte.net>
2025-07-29 14:57:58 +0200

    Change return type of async_generator to Generator for consistency

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields 10 random floats between 0-10 with 1s delay between each."""
     for _ in range(10):
         await asyncio.sleep(1)
