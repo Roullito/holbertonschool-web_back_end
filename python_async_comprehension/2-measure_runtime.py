43d26f111bd8687c91149bffc853354d4aa610e4
Roullito <jmoleins@laposte.net>
2025-07-29 14:33:35 +0200

    Add type annotation to measure_runtime function

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Measure the total runtime of executing async_comprehension 4 times in parallel.
 
