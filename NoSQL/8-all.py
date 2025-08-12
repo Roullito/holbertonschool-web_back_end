48c0872f83a1fb1fc5ba2edc090abaeabfcbef9a
Roullito <jmoleins@laposte.net>
2025-08-12 14:05:01 +0200

    Refactor list_all function documentation to remove unnecessary argument details

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
+"""
+module for listing mongodb doc
+"""
 
 def list_all(mongo_collection):
         """
         Lists all documents in a MongoDB collection.
-
-        Args:
-                mongo_collection: pymongo collection object
-
-        Returns:
-                List of documents in the collection
         """
         return list(mongo_collection.find())
 
