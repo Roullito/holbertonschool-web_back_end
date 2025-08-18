403ebcffb73814f098acc560a6df6be68c1cd68f
Roullito <jmoleins@laposte.net>
2025-08-18 13:40:05 +0200

    Fix parameter name from perCapita to Capita in getBudgetObject function

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,3 @@
-export default function getBudgetObject(income, gdp, perCapita) {
-  return { income, gdp, perCapita };
+export default function getBudgetObject(income, gdp, Capita) {
+  return { income, gdp, Capita };
 }
