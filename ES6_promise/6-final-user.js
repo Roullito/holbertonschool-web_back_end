cace6c8273cf00bfd8f5d4dda9b48fdce1c51dcd
Roullito <jmoleins@laposte.net>
2025-08-26 14:19:45 +0200

    Refactor handleProfileSignup function to improve promise handling and formatting

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,15 @@
-import signUpUser from "./4-user-promise.js"
-import uploadPhoto from "./5-photo-reject.js"
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName),
-    ]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((r) => (
+      r.status === 'fulfilled'
+        ? { status: r.status, value: r.value }
+        : { status: r.status, value: String(r.reason) }
+    ))
+  );
 }
