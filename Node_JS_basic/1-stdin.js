b2385bb9a257350eded388d531e8a47964ea6e8c
Roullito <jmoleins@laposte.net>
2025-09-01 12:04:53 +0200

    Refactor stdin.js to simplify input handling and streamline closing message logic

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,10 @@
+
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
-let inputReceived = false;
-
 process.stdin.on('data', (chunk) => {
-  if (!inputReceived) {
     const name = chunk.trim();
     console.log(`Your name is: ${name}`);
-    inputReceived = true;
-    if (process.stdin.isTTY) {
-      process.stdin.resume();
-    } else {
-      process.stdin.emit('end');
-    }
-  }
-});
-
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+    console.log('This important software is now closing');
+    process.exit();
 });
