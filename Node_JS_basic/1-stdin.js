b68132b460e2aa3c6dba9730f56eafb30e419ab5
Roullito <jmoleins@laposte.net>
2025-09-01 12:00:50 +0200

    Refactor stdin.js to improve input handling and remove trailing period in closing message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,21 @@
 console.log('Welcome to Holberton School, what is your name?');
-
 process.stdin.setEncoding('utf8');
 
+let inputReceived = false;
+
 process.stdin.on('data', (chunk) => {
-  const name = chunk.trim();
-  console.log(`Your name is: ${name}`);
+  if (!inputReceived) {
+    const name = chunk.trim();
+    console.log(`Your name is: ${name}`);
+    inputReceived = true;
+    if (process.stdin.isTTY) {
+      process.stdin.resume();
+    } else {
+      process.stdin.emit('end');
+    }
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing.');
+  console.log('This important software is now closing');
 });
