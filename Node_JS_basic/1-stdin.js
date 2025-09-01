2d434a01322d31bef761339e3ae3d0665fbed29a
Roullito <jmoleins@laposte.net>
2025-09-01 11:56:08 +0200

    Refactor stdin.js to replace process.stdout.write with console.log for consistency

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (chunk) => {
   const name = chunk.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing.\n');
+  console.log('This important software is now closing.');
 });
